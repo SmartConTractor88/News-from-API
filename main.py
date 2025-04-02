f570bf2c2a15f811616ceac0feed61a007f54ede
SmartConTractor <login.blockchain888@gmail.com>
2025-04-02 10:59:53 +0200

    Extract article title, description and link.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 #from send_email import send_email
 
-
 api_key = "pub_77528381bf43230c1877b14e6739ac620b6da"
 
 # define url as str
@@ -12,4 +11,31 @@ url = str("https://newsdata.io/api/1/"\
 request = requests.get(url) # make request
 content = request.json() # convert the content into a json format dictionary
 
-print(content)
+# access the value of 'results' key
+articles = content['results']
+
+# iterate over all elements in the 'articles' list
+for index, article in enumerate(articles):
+    print()
+    print(f"{index+1}. ARTICLE")
+
+    # article title
+    print(article['title'])
+    print()
+
+    # if description is a nonetype, invent it
+    if article['description'] == None:
+        description = "No description available. CLick the link to read more."
+
+    # if it's longer that 150, only show first 150 characters
+    elif len( str(article['description']) ) > 150:
+        description = f"{str(article['description']) [:150]}..."
+
+    # if it's not greater that 150, show all of it
+    elif len(article['description']) <= 150:
+        description = str(article['description'])
+
+    print( f"{description}" )      
+
+    # show the link
+    print(f"Link: {article['link']}")
